2079d7c7529e750b7b56cc4c76162dc75b53d946
谷口 <hmj2422032@stu.o-hara.ac.jp>
2025-09-16 11:36:32 +0900

    9/16　アセット導入

---
+++ Assets/Enemy/Script/EnemyMove.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+	[SerializeField]
+	GameObject target;
+
+
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
