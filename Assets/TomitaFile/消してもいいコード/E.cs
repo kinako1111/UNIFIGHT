1dba24078ed02b56e2b8a7228bdd99c4914f37a3
hmj2422001 <hmj2422001@stu.o-hara.ac.jp>
2025-09-25 12:39:52 +0900

    マップ2つ目完成と敵の動きの基盤を作った。

---
+++ Assets/TomitaFile/消してもいいコード/E.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class E : MonoBehaviour
+{
+	private enum TargetType
+	{
+		Tower,
+		Player,
+	}
+
+	[Header("Navmesh"), SerializeField]
+	NavMeshAgent m_navmeshAgent;
+
+	[Header("–h‰q‘ÎÛ"), SerializeField]
+	GameObject[] m_target;
+
+	[SerializeField]
+	Animator m_animator;
+
+	float m_attackCoolTime;
+	bool isMove;
+
+	private void Start()
+	{
+		//if (m_target == null)
+		//{
+		//	m_target = GameObject.FindGameObjectWithTag("Target");
+		//}
+	}
+
+	private void FixedUpdate()
+	{
+		// “G‚©‚çŒ©‚Ätower‚Ì•û‚ªPlayer‚æ‚è‚à‰“‚©‚Á‚½‚çPlyer‚ğ’ÇÕ‚·‚é
+		if ((m_target[((int)TargetType.Tower)].transform.position - transform.position).magnitude >=
+			(m_target[((int)TargetType.Player)].transform.position - transform.position).magnitude) 
+		{
+			//Navmesh‚ÌˆÚ“®
+			m_navmeshAgent.SetDestination(m_target[((int)TargetType.Player)].transform.position);
+			isMove = true;
+		}
+		// “G‚©‚çŒ©‚Ätower‚Ì•û‚ªPlayer‚æ‚è‚à‹ß‚¢‚©‚Â“G‚ÆPlayer‚Æ‚Ì‹——£‚ª10–¢–‚¾‚Á‚½‚çtower‚ğ’ÇÕ‚·‚é
+		else if ((m_target[((int)TargetType.Tower)].transform.position - transform.position).magnitude <=
+				(m_target[((int)TargetType.Player)].transform.position - transform.position).magnitude ||
+				(m_target[((int)TargetType.Player)].transform.position - transform.position).magnitude <= 10)
+		{
+			//Navmesh‚ÌˆÚ“®
+			m_navmeshAgent.SetDestination(m_target[((int)TargetType.Tower)].transform.position);
+
+			isMove = true;
+		}
+		else 
+		{
+			isMove = false;
+		}
+
+		if (!isMove)
+		{
+			m_attackCoolTime += Time.deltaTime;
+			if (m_attackCoolTime >= 4)
+			{
+				OnAttack();
+			}
+		}
+
+		m_animator.SetBool("Walk", isMove);
+		Debug.Log(isMove);
+	}
+
+	void OnAttack()
+	{
+		m_animator.SetTrigger("Attack");
+		m_attackCoolTime = 0;
+	}
+}
