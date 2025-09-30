c4e2d6747cd54e1817fe75e7c909495fbc4484be
mm5_work-admin <hmj2422004@stu.o-hara.ac.jp>
2025-09-30 14:35:06 +0900

    キャラ１【ショットガン】弾作成等

---
+++ Assets/Hatanaka/ShotGunBullet.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotGunBullet : MonoBehaviour
+{
+    [SerializeField] float damage;
+    [SerializeField] float bulletSpeed;
+    [SerializeField] float deleteTime;
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+	public void SetDirection(Vector3 direction)
+	{
+		// íeÇ™î≠éÀÇ≥ÇÍÇΩï˚å¸Ç…êiÇﬁÇÊÇ§Ç…ê›íË
+		transform.forward = direction;
+	}
+	// Update is called once per frame
+	void Update()
+    {
+		transform.Translate(Vector3.forward * bulletSpeed * Time.deltaTime);
+		deleteTime -= Time.deltaTime;
+        damage -= Time.deltaTime;
+        if(deleteTime<0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if(CompareTag("Enemy"))
+        {
+			Destroy(other.gameObject); // ìGÇîjâÛ
+			Destroy(gameObject); // íeÇîjâÛ
+		}
+	}
+}
