85268470d17ca536887f3e74bf47d1594978eca2
谷口 <hmj2422032@stu.o-hara.ac.jp>
2025-10-08 12:16:09 +0900

    Revert "敵のスクリプト、Playerの動きの一部"
    This reverts commit e55a577f50ea89567235d2ba963419f9bc94f9a4.

--- Assets/TaniguchiFile/EnemyAction.cs
+++ Assets/TaniguchiFile/EnemyAction.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.AI;
@@ -23,12 +22,6 @@ public class EnemyAction: MonoBehaviour
 	[Header("サウンドのリスト"),SerializeField]
 	List<GameObject> m_soundList = new();
 
-	[Header("敵の速度"), SerializeField]
-	float m_speed;
-
-	[Header("攻撃範囲"), SerializeField]
-	float AttackRange;
-
 	//防衛対象のリスト
 	List<GameObject> m_targetList = new();
 
@@ -42,14 +35,13 @@ public class EnemyAction: MonoBehaviour
 	Animator m_animator;
 	float m_coolTime;
 	bool isMove;
-	bool isAttack = false;
 
 	private void Awake()
 	{
 		m_animator = GetComponent<Animator>();
+
 		m_playerList.AddRange(GameObject.FindGameObjectsWithTag("Player"));
 		m_targetList.AddRange(GameObject.FindGameObjectsWithTag("Target"));
-		m_targetList.AddRange(GameObject.FindGameObjectsWithTag("Player"));
 	}
 
 	private void FixedUpdate()
@@ -57,26 +49,22 @@ public class EnemyAction: MonoBehaviour
 		m_navmeshAgent.speed = status.GetSpeed();
 		m_attackCoolTime = status.GetSkill1CoolTime();
 
-		//現在最も近いターゲットを取得
-		GameObject clossTarget = m_targetList.OrderBy(target => Vector3.Distance(target.transform.position, transform.position)).First();
+		//現在最も近いプレイヤーを取得
+		GameObject clossPlayer = m_playerList.OrderBy(target => Vector3.Distance(target.transform.position, transform.position)).First();
 
 		//攻撃範囲内にプレイヤーがいる場合
-		if ((clossTarget.transform.position - transform.position).magnitude <= AttackRange)
+		if ((clossPlayer.transform.position - transform.position).magnitude <= m_navmeshAgent.stoppingDistance
+			||(m_targetList.First().transform.position - transform.position).magnitude <= m_navmeshAgent.stoppingDistance)
 		{
 			isMove = false;
-			m_navmeshAgent.isStopped = true;
 
-			//攻撃中は向きも変わらない
-			if(!isAttack)
-			{
-				transform.rotation = Quaternion.Lerp(transform.rotation,Quaternion.LookRotation(clossTarget.transform.position - transform.position),0.2f);
-			}
+			//対象物を最もヘイト値の高いプレイヤーに変更
+			//ヘイト値　：　攻撃範囲-プレイヤーの距離＋個々の値
+			//攻撃範囲内のプレイヤー取得　ー＞　ヘイト値計算
 
-			////対象物を最もヘイト値の高いプレイヤーに変更
-			////ヘイト値　：　攻撃範囲-プレイヤーの距離＋個々の値
-			////攻撃範囲内のプレイヤー取得　ー＞　ヘイト値計算
+			List<GameObject> playerList = m_playerList.FindAll(target => Vector3.Distance(target.transform.position ,transform.position) > m_navmeshAgent.stoppingDistance);
+				
 
-			//List<GameObject> playerList = m_playerList.FindAll(target => Vector3.Distance(target.transform.position ,transform.position) > m_navmeshAgent.stoppingDistance);
 
 			//クールタイム終了と同時に攻撃
 			m_coolTime -= Time.deltaTime;
@@ -88,36 +76,35 @@ public class EnemyAction: MonoBehaviour
 		else
 		{
 			isMove = true;
-
-			//攻撃中は移動禁止
-			if(!isAttack)
-			{
-				m_navmeshAgent.isStopped = false;
-				m_navmeshAgent.SetDestination(m_targetList.Find(target => target.tag == "Target").transform.position);
-			}
+			m_navmeshAgent.SetDestination(m_targetList.First().transform.position);
 		}
 		m_animator.SetBool("Walk", isMove);
 	}
 
 	void OnAttack()
 	{
-		isAttack = true;
 		m_animator.SetTrigger("Attack");
 		m_coolTime = m_attackCoolTime;
 	}
 
-	public void AttackHit()
+
+
+	public void AttackStart()
 	{
 		Debug.Log("Golem attack started!");
 		// エフェクト生成やSE再生などの処理
+
 		GameObject obj = Instantiate(m_effectList.First(), m_attackPos.First());
+		Debug.Log(obj);
 	}
+
 
 	public void AttackEnd()
 	{
-		Debug.Log("Golem attack end!");
-		isAttack = false;
+
 	}
+
+
 
 	public void SetTarget(GameObject target)
 	{
--- Assets/TaniguchiFile/EnemyFactory.cs
+++ Assets/TaniguchiFile/EnemyFactory.cs
@@ -8,6 +8,7 @@ public class EnemyFactory : MonoBehaviour
 	private enum EnemyName
 	{
 		Golem,
+	
 	}
 
 	[SerializeField]
