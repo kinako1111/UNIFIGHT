0c03710aa62c2301d5845055e180d7dec79dcb68
重川由雅 <hmj2422005@stu.o-hara.ac.jp>
2025-10-10 15:11:24 +0900

    1PSceneでダメージUI生成途中

--- Assets/Shigekawa/UI/HitBoxControl.cs
+++ Assets/Shigekawa/UI/HitBoxControl.cs
@@ -10,7 +10,7 @@ public class HitBoxControl : MonoBehaviour
 
 	private void OnTriggerEnter(Collider other)
 	{
-		if(other.gameObject.CompareTag("Bullet"))
+		if (other.gameObject.CompareTag("Bullet"))
 		{
 			PopDamageUI();
 		}
@@ -18,20 +18,19 @@ public class HitBoxControl : MonoBehaviour
 
 	void PopDamageUI()
 	{
-		var obj = new GameObject("Target");
+		var obj = new GameObject("DamageTargetPosition");
 		var ui = Instantiate(_damageUI);
 
-		obj.transform.SetParent(_targets.transform);		// ターゲット＆テキストUIの親をそれぞれ変更
+		obj.transform.SetParent(_targets.transform);
 		ui.transform.SetParent(_canvas.transform);
 
 		ui.SetActive(true);
 
 		var circlePos = Random.insideUnitCircle * 1.2f;
-		obj.transform.position = transform.position + Vector3.up * Random.Range(3.0f, 4.0f) + new Vector3(circlePos.x, 0, circlePos.y); // ターゲットの位置をランダムで指定する
-		ui.GetComponent<RectTransform>().position = RectTransformUtility.WorldToScreenPoint(Camera.main,obj.transform.position);		// + UIの位置をターゲットの画面上の位置に指定する
+		obj.transform.position = transform.position + Vector3.up * Random.Range(3.0f, 4.0f) + new Vector3(circlePos.x, 0, circlePos.y);
+		ui.GetComponent<RectTransform>().position = RectTransformUtility.WorldToScreenPoint(Camera.main, obj.transform.position);
 
 		Destroy(obj, 5.0f);
 		Destroy(ui, 5.0f);
 	}
-
 }
