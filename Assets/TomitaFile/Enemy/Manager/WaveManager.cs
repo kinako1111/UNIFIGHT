2385c133e296c8e37956786f21d55855b9d00ffa
重川由雅 <hmj2422005@stu.o-hara.ac.jp>
2025-10-31 10:07:15 +0900

    WaveManagerがバグったので修正した

--- Assets/TomitaFile/Enemy/Manager/WaveManager.cs
+++ Assets/TomitaFile/Enemy/Manager/WaveManager.cs
@@ -6,23 +6,23 @@ using UnityEngine;
 
 public class WaveManager : MonoBehaviour
 {
-    [SerializeField] TextMeshProUGUI m_waveText;
-    [SerializeField] GameObject m_textActive;
+	[SerializeField] TextMeshProUGUI m_waveText;
+	[SerializeField] GameObject m_textActive;
 
-    private int m_waveCount = 0;
+	private int m_waveCount = 0;
 
-    public void WaveCount()
-    {
-        m_waveCount++;
+	public void WaveCount()
+	{
+		m_waveCount++;
 		m_textActive.SetActive(true);
 		m_waveText.text = "現在のウェーブは" + m_waveCount.ToString() + "です";
 		StartCoroutine(WaveCountRead());
 
 	}
 
-    IEnumerator WaveCountRead()
-    {
-        yield return new WaitForSeconds(3);
-        m_textActive.SetActive(false);
+	IEnumerator WaveCountRead()
+	{
+		yield return new WaitForSeconds(3);
+		m_textActive.SetActive(false);
 	}
 }
